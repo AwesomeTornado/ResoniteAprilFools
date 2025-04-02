7d457e68520f7cba4c202e85477792535a1fa87a
Delta <delta@deltawolf.net>
2025-04-02 11:53:12 -0700

    Use getter annotation on patch

--- ResoniteAprilFools/ResoniteAprilFools.cs
+++ ResoniteAprilFools/ResoniteAprilFools.cs
@@ -5,7 +5,7 @@ using ResoniteModLoader;
 namespace ResoniteAprilFools;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 public class ResoniteAprilFools : ResoniteMod {
-	internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "1.0.1"; //Changing the version here updates it in all locations needed
 	public override string Name => "ResoniteAprilFools";
 	public override string Author => "__Choco__";
 	public override string Version => VERSION_CONSTANT;
@@ -13,15 +13,13 @@ public class ResoniteAprilFools : ResoniteMod {
 
 	public override void OnEngineInit() {
 		Harmony harmony = new Harmony("com.__Choco__.ResoniteAprilFools");
-
-		harmony.Patch(AccessTools.Method(typeof(Engine), "<get_IsAprilFools>g__IsAprilFools|2_0"), prefix: AccessTools.Method(typeof(AprilFoolsPatch), "Prefix"));
-
 		harmony.PatchAll();
 
 		Msg("Resonite April Fools has initialized");
 		Msg("Enjoy the tomfoolery!");
 	}
 
+	[HarmonyPatch(typeof(Engine), "IsAprilFools", MethodType.Getter)]
 	class AprilFoolsPatch{
 		static bool Prefix(ref bool __result) {
 			__result = true;
